8aa3bd2923e822bda96ddf32471e3e08aff1eb0d
Jenny Chang <jennychang@Jennys-MacBook-Pro-2.local>
2019-02-16 15:37:00 -0800

    added edit and delete functionality

---
+++ FoodTracker/AddMealViewController.swift
@@ -0,0 +1,125 @@
+import UIKit
+import os.log
+
+class AddMealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var meal: Meal?
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var foodPicView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+  
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameTextField.delegate = self
+        
+    // enable save only if the text field has a valid Meal name
+        updateSaveButtonState()
+    }
+    
+    @IBAction func cancelButton(_ sender: Any) {
+        
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        
+        // if meal detail scene is in add meal mode
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+            
+        // if user is editing an existing meal then safely unwrap the navigation controller
+        } else if let owningNavigationController = navigationController {
+            owningNavigationController.popViewController(animated: true)
+            
+        // shouldn't be executed if code is written properly
+        } else {
+            fatalError("the AddMealViewController is not inside a navigation controller")
+        }
+    }
+    
+//////////////////////////////////////////////////////////
+//    configure the destination view controller only when the save button is pressed
+// in storyboard, we dragged the save button to the 'exit' button
+// this links to "unwind to meal list in MealTableViewController"
+////////////////////////////////////////////////////////
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+
+        
+        guard let button = sender as? UIBarButtonItem, button === saveButton else {
+            os_log("the save button was not pressed, cancelling", log: OSLog.default, type: .debug)
+            return
+        }
+        
+        let name = nameTextField.text ?? ""
+        let photo = foodPicView.image
+        let rating = ratingControl.rating
+        
+        meal = Meal(name: name, photo: photo, rating: rating)
+    }
+    
+//////////////////////////////////////////////////////////
+//    reverse segue - add an action to the destination view controller (the one we want to segue to)
+////////////////////////////////////////////////////////
+    
+    @IBAction func selectImageFromLibrary(_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        
+        let iPC = UIImagePickerController()
+        iPC.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        iPC.sourceType = .photoLibrary
+        
+        
+        present(iPC, animated: true, completion: nil)
+        
+    }
+    
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        // disable save button while editing
+        saveButton.isEnabled = false
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+        updateSaveButtonState()
+        navigationItem.title = textField.text
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // hides keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    
+//////////////////////////////////////////////////////////
+//    disable save button when text field is empty
+////////////////////////////////////////////////////////
+    
+    private func updateSaveButtonState() {
+        let text = nameTextField.text ?? ""
+        saveButton.isEnabled = !text.isEmpty
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            fatalError("expected a dictionary containing an image, but it provided the following: \(info)")
+        }
+        
+        foodPicView.contentMode = .scaleAspectFit
+        foodPicView.image = selectedImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
+
---
+++ FoodTracker/MealTableViewController.swift
@@ -0,0 +1,108 @@
+import UIKit
+
+class MealTableViewController: UITableViewController {
+
+    var meals = [Meal]()
+    var meal: Meal!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // goes into editing mode on its own!!
+        navigationItem.leftBarButtonItem = editButtonItem
+        loadSampleMeals()
+        
+    }
+    
+//////////////////////////////////////////////////////////
+//    reverse segue - receiving from AddMealViewController
+////////////////////////////////////////////////////////
+    
+    @IBAction func unwindToMealList(sender: UIStoryboardSegue) {
+        
+        if let sourceViewController = sender.source as? AddMealViewController, let meal = sourceViewController.meal {
+            
+
+            let newIndexPath = IndexPath(row: meals.count, section: 0)
+            meals.append(meal)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
+        }
+        
+    }
+
+//////////////////////////////////////////////////////////
+//    setting up table view
+////////////////////////////////////////////////////////
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellID = "cellID"
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as? MealTableViewCell else {
+            fatalError("dequeued cell is not an instance of MealTableViewCell")
+        }
+        
+        let meal = meals[indexPath.row]
+        
+        cell.nameLabel.text = meal.name
+        cell.photoImageView.image = meal.photo
+        cell.photoImageView.contentMode = .scaleToFill
+        cell.ratingControl.rating = meal.rating
+        
+        return cell
+    }
+ 
+//////////////////////////////////////////////////////////
+//    allow user to edit --> delete
+////////////////////////////////////////////////////////
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            meals.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // create a new instance
+        }
+    }
+    
+//////////////////////////////////////////////////////////
+//    return false if you don't want item to be editable
+////////////////////////////////////////////////////////
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+//////////////////////////////////////////////////////////
+//    loading default
+////////////////////////////////////////////////////////
+    
+    private func loadSampleMeals() {
+        let photo1 = UIImage(named: "meal1")
+        let photo2 = UIImage(named: "meal2")
+        let photo3 = UIImage(named: "meal3")
+        
+        guard let meal1 = Meal(name: "Caprese salad", photo: photo1, rating: 4) else {
+            fatalError("unable to instantiate meal1")
+        }
+        
+        guard let meal2 = Meal(name: "Chicken and potatoes", photo: photo2, rating: 5) else {
+            fatalError("unable to instantiate meal2")
+        }
+        
+        guard let meal3 = Meal(name: "Pasta with meatballs", photo: photo3, rating: 3) else {
+            fatalError("unable to instantiate meal3")
+        }
+        
+        meals.append(contentsOf: [meal1, meal2, meal3])
+    }
+}
